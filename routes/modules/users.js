b1f07f6adf692341fda9c121f794f6494e381f6b
vdiefn <ddhkxxhxx@hotmail.com>
2023-02-14 18:13:05 +0800

    modify register function

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const router = express.Router()
-const User = require('../../models/user')
+
 const passport = require('passport')
 const bcrypt = require('bcryptjs')
+
+const db = require('../../models')
+const User = db.User
 
 
 //登入
